25b871200ac9d10226e77fbc272c512e1b67ab68
keishasy <163233331+keishasy@users.noreply.github.com>
2024-11-29 19:03:33 +0700

    Create MakhlukHidup.java

---
+++ Abstract1/MakhlukHidup.java
@@ -0,0 +1,8 @@
+abstract class MakhlukHidup {
+    public abstract void bernapas();
+    public abstract void makan();
+
+    public void berkembangBiak() {
+        System.out.println("Makhluk hidup berkembang biak.");
+    }
+}
