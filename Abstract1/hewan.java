6ce648b4e95767c5fbb5c7fdcb06689811fdf9ec
keishasy <163233331+keishasy@users.noreply.github.com>
2024-11-29 18:57:47 +0700

    Create hewan.java

---
+++ Abstract1/hewan.java
@@ -0,0 +1,16 @@
+class Hewan extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Hewan bernapas dengan paru-paru atau insang.");
+    }
+
+    @Override
+    public void makan() {
+        System.out.println("Hewan makan dengan mulut.");
+    }
+
+    @Override
+    public void berkembangBiak() {
+        System.out.println("Hewan berkembang biak dengan bertelur atau melahirkan.");
+    }
+}
