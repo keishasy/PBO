c72eb080e57a02508a0c217ed1b484e8395af39f
keishasy <163233331+keishasy@users.noreply.github.com>
2024-11-29 19:21:46 +0700

    Create tumbuhan.java

---
+++ Abstract1/tumbuhan.java
@@ -0,0 +1,16 @@
+class Tumbuhan extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Tumbuhan bernapas melalui stomata.");
+    }
+
+    @Override
+    public void makan() {
+        System.out.println("Tumbuhan membuat makanan sendiri melalui fotosintesis.");
+    }
+
+    @Override
+    public void berkembangBiak() {
+        System.out.println("Tumbuhan berkembang biak dengan biji, tunas, atau stek.");
+    }
+}
